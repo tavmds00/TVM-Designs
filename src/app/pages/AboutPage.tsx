271612c31fe5a5fb74c0bf1ac1ddfd4e5262f148
mas <tomasvmoreno@gmail.com>
2026-06-09 16:02:19 -0500

    fix about folder name

--- src/app/pages/AboutPage.tsx
+++ src/app/pages/AboutPage.tsx
@@ -1,6 +1,7 @@
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
-import section1 from "../../portfolio-pages/about/section1.png";
+import section1 from "../../../portfolio-pages/about/section1.png";
+
 export function AboutPage() {
   return (
     <div className="min-h-screen bg-black text-white">

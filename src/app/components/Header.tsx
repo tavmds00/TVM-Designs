d65ae395542426a2e5f31365e92ed40063977b13
mas <tomasvmoreno@gmail.com>
2026-06-16 17:01:23 -0500

    “headerfix”

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -47,7 +47,7 @@ export function Header() {
 
   return (
     <>
-      <header className="fixed top-0 left-0 right-0 z-100 bg-black text-white border-b border-neutral-900">
+      <header className="fixed top-0 left-0 right-0 z-[300] bg-black text-white border-b border-neutral-900">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-14 sm:h-16">
             <div className="flex items-center">
